ea6206d9a25a45e5964b34fef44cde6a1017dd7c
Hayford Bethann <hbethann@at-n3.msk.21-school.ru>
2022-03-23 19:11:07 +0300

    pre-finish 2

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hbethann <hbethann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 14:51:51 by hbethann          #+#    #+#             */
-/*   Updated: 2022/03/12 14:56:41 by hbethann         ###   ########.fr       */
+/*   Updated: 2022/03/23 18:48:45 by hbethann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,63 @@ char	**ft_line(char **argv);
 void	check_arg(char **array);
 void	free_array(char **array, int flag);
 void	error(void);
+
+// add_stack.c
+void	stack_add(t_stack *stack, char **arr);
+void	bubble_sorting(t_stack *stack, char *sort_arr,
+			char *not_sort_arr, int len);
+void	create_stack(t_stack *stack, char *sort_arr,
+			char *not_sort_arr, int len);
+
+// swap.c
+void	sa(t_stack *stack, int flag);
+void	sb(t_stack *stack, int flag);
+void	ss(t_stack *stack, int flag);
+
+// push_cmd.c
+void	push_b(t_stack *stack);
+void	push_a(t_stack *stack);
+void	pb(t_stack *stack, int flag);
+void	pa(t_stack *stack, int flag);
+
+// rotate.c
+void	ra(t_stack *stack, int flag);
+void	rb(t_stack *stack, int flag);
+void	rr(t_stack *stack, int flag);
+
+// rev_rot.c
+void	rra(t_stack *stack, int flag);
+void	rrb(t_stack *stack, int flag);
+void	rrr(t_stack *stack, int flag);
+
+// minisort.c
+int		sort_true(t_stack *stack);
+void	sort_three_a(t_stack *stack);
+void	sort_three_b(t_stack *stack);
+int		shor_cut(t_stack *stack);
+void	five_sort(t_stack *stack);
+
+// medsort.c
+void	min_med_max_a(t_stack *stack);
+void	min_med_max_b(t_stack *stack);
+void	move_down(t_stack *stack);
+void	move_a_to_b_first(t_stack *stack);
+
+// big_sort.c
+void	push_down(t_stack *stack);
+int		search_min_b_to_a(t_stack *stack);
+void	move_b_to_a(t_stack *stack);
+void	search_min_a_to_b(t_stack *stack);
+void	move_a_to_b(t_stack *stack);
+
+// optimus.c
+int		three_el(t_stack *stack);
+void	five_el_first_ab(t_stack *stack, int flags);
+void	five_el_second_ab(t_stack *stack, int flags, int min, int i);
+void	five_el_first_ba(t_stack *stack, int flags);
+void	five_el_second_ba(t_stack *stack);
+
+// cheker.c
+t_stack	*parser(char **argv);
 
 #endif
